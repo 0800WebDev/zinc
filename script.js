e8cf2ecdac80de6cea37a0a128d5c6501850b5b0
0800 <spmspy0800@gmail.com>
2026-09-16 17:25:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2696,13 +2696,25 @@ function initializeAutoswitchSetting() {
 function toggleDevTools() {
     const win = getActiveTab()?.frame.frame.contentWindow;
     if (!win) return;
+
     if (win.eruda) {
-        win.eruda.show();
+        if (win.eruda._isShow) {
+            win.eruda.hide();
+            win.eruda.get('entryBtn').hide();
+        } else {
+            win.eruda.get('entryBtn').show();
+            win.eruda.show();
+        }
         return;
     }
+
     const script = win.document.createElement('script');
     script.src = "https://cdn.jsdelivr.net/npm/eruda";
-    script.onload = () => { win.eruda.init(); win.eruda.show(); };
+    script.onload = () => {
+        win.eruda.init();
+        win.eruda.get('entryBtn').hide();
+        win.eruda.show();
+    };
     win.document.body.appendChild(script);
 }
 
